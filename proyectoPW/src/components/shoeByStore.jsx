d9bf70c0f5673c9016e4e2dbd6a20f17919c8d55
AngelPabloSC <138425945+AngelPabloSC@users.noreply.github.com>
2024-08-16 05:57:07 -0500

    todok
    valio la pena

--- proyectoPW/src/components/shoeByStore.jsx
+++ proyectoPW/src/components/shoeByStore.jsx
@@ -1,52 +1,72 @@
-import React from 'react';
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const ShoeByStore = () => {
-    return (
-        <div className="bg-cover bg-[url('https://i.pinimg.com/736x/09/8b/02/098b02b3138104e753440a7655ac38d5.jpg')] min-h-screen py-12">
-            {/* Sección de bienvenida */}
-            <div className="bg-black shadow-md rounded-3xl max-w-6xl mx-auto p-8 ">
+  const location = useLocation();
+  const { storeData } = location.state || { storeData: [] };
 
-                <div className='flex flex-row items-center py-6 space-x-8'>
-                    <img className="h-28 rounded-full border-2 border-[#fcf149]"
-                        src="https://t4.ftcdn.net/jpg/01/36/55/49/360_F_136554929_JG7RLQNfAKpAQlmRmdV7QhbEM1PDt6k2.jpg"
-                        alt="" />
-                    <h1 className="text-5xl font-extrabold text-center text-[#fcf149]">
-                        Bienvenido a la tienda "nameStore"
-                    </h1>
-                </div>
-                <div>
-                    <p className="text-lg text-gray-200 text-center my-8">
-                        En "nameStore", nos dedicamos a ofrecerte los mejores zapatos para cualquier ocasión.
-                        Explora nuestra colección y encuentra el par perfecto que se ajuste a tu estilo y comodidad.
-                    </p>
-                </div>
-            </div>
-
-            <div className='relative flex flex-col items-center py-20 sm:px-0 px-6'>
-                <div className="relative z-10">
-                    <h1
-                        id="tituloo"
-                        className="text-4xl sm:text-5xl text-center font-extrabold tracking-tight leading-none text-[#fcf149] relative">
-                        Nuestra colección
-                    </h1>
-                    <span className="absolute inset-0 bg-black opacity-100 blur-md rounded-lg -z-10"></span>
-                    <span className="absolute inset-0 bg-black opacity-100 blur-md rounded-lg scale-105 transform -z-20"></span>
-                </div>
-            </div>
-
-            <div className="flex flex-wrap justify-center gap-8 pb-16 sm:px-48 px-0">
-                    <div className="w-60 bg-black border border-[#fcf149] rounded-lg shadow" key="">
-                        <img className="p-8" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkuF_GL_laAsOYQr17AUbeTTgl-MP12fQfXw&s" alt="" />
-                        <h5 className="mx-4 mb-4 text-2xl text-center font-semibold tracking-tight text-[#fcf149]">Nike Air Jordan</h5>
-                        <div className="px-6">
-                            <h5 className="text-base font-semibold tracking-tight text-white"> <span className='text-gray-400'>Marca:</span> "#"</h5>
-                            <h5 className="text-3xl text-center font-bold text-green-700 pb-8"><br /> $99.99</h5>
-                        </div>
-                    </div>
-            </div>
-
+  return (
+    <div className="bg-cover bg-[url('https://i.pinimg.com/736x/09/8b/02/098b02b3138104e753440a7655ac38d5.jpg')] min-h-screen py-12">
+      {/* Sección de bienvenida */}
+      <div className="bg-black shadow-md rounded-3xl max-w-6xl mx-auto p-8 ">
+        <div className="flex flex-row items-center py-6 space-x-8">
+          <img
+            className="h-28 rounded-full border-2 border-[#fcf149]"
+            src="https://t4.ftcdn.net/jpg/01/36/55/49/360_F_136554929_JG7RLQNfAKpAQlmRmdV7QhbEM1PDt6k2.jpg"
+            alt=""
+          />
+          <h1 className="text-5xl font-extrabold text-center text-[#fcf149]">
+            Bienvenido a la tienda "
+            {storeData.length > 0 ? storeData[0].store_name : "Desconocido"}"
+          </h1>
         </div>
-    );
+        <div>
+          <p className="text-lg text-gray-200 text-center my-8">
+            En "{storeData.length > 0 ? storeData[0].store_name : "Desconocido"}
+            ", nos dedicamos a ofrecerte los mejores zapatos para cualquier
+            ocasión. Explora nuestra colección y encuentra el par perfecto que
+            se ajuste a tu estilo y comodidad.
+          </p>
+        </div>
+      </div>
+
+      <div className="relative flex flex-col items-center py-20 sm:px-0 px-6">
+        <div className="relative z-10">
+          <h1
+            id="tituloo"
+            className="text-4xl sm:text-5xl text-center font-extrabold tracking-tight leading-none text-[#fcf149] relative"
+          >
+            Nuestra colección
+          </h1>
+          <span className="absolute inset-0 bg-black opacity-100 blur-md rounded-lg -z-10"></span>
+          <span className="absolute inset-0 bg-black opacity-100 blur-md rounded-lg scale-105 transform -z-20"></span>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-8 pb-16 sm:px-48 px-0">
+        {storeData.map((shoe) => (
+          <div
+            className="w-60 bg-black border border-[#fcf149] rounded-lg shadow"
+            key={shoe.shoe_id}
+          >
+            <img className="p-8" src={shoe.image_url} alt={shoe.shoe_name} />
+            <h5 className="mx-4 mb-4 text-2xl text-center font-semibold tracking-tight text-[#fcf149]">
+              {shoe.shoe_name}
+            </h5>
+            <div className="px-6">
+              <h5 className="text-base font-semibold tracking-tight text-white">
+                {" "}
+                <span className="text-gray-400">Marca:</span> {shoe.brand_name}
+              </h5>
+              <h5 className="text-3xl text-center font-bold text-green-700 pb-8">
+                <br /> ${shoe.price}
+              </h5>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ShoeByStore;
--- proyectoPW/src/components/storesForm.jsx
+++ proyectoPW/src/components/storesForm.jsx
@@ -1,66 +1,89 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useFetchData } from "../hooks/useFectchData";
-import Loading from './messages/loading';
-import ErrorMessage from './messages/errorMessage';
-import Message from './messages/messages';
-
+import Loading from "./messages/loading";
+import ErrorMessage from "./messages/errorMessage";
+import Message from "./messages/messages";
+import { useFetchDataPromise } from "../hooks/useFectchDataPromise";
 const StoresForm = () => {
-    const { fetchData: { loading, code, message, data } } = useFetchData({ endPoint: "store/getAll" });
-    
-    if (loading) return <Loading/>;
-    if (code !== "COD_OK") return <ErrorMessage message={message} />;
-    if (data.length === 0) return <Message message="No hay tiendas disponibles en este momento." />;
+  const { getFetchData } = useFetchDataPromise();
+  const navigate = useNavigate();
+  const {
+    fetchData: { loading, code, message, data },
+  } = useFetchData({ endPoint: "store/getAll" });
 
-    return (
-        <div className='content-center sm:px-48 sm:py-12'>
-            <hr className='border-black' />
+  const handleStoreClick = async (store_id) => {
+    const response = await getFetchData({
+      endPoint: `storeshoes/${store_id}`,
+      method: "POST",
+      additionalData: {
+        store_id: store_id,
+      },
+    });
 
-            <div className='flex items-center justify-center py-8'>
-                <button className="overflow-hidden relative w-72 p-2 h-20 bg-black text-[#faef57] border-none rounded-lg text-4xl font-bold cursor-pointer z-10 group">
-                    TIENDAS
-                    <span
-                        className="absolute w-96 h-60 -top-32 -left-8 bg-[#faef57] rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-left"
-                    ></span>
-                    <span
-                        className="text-black group-hover:opacity-100 pl-4 p-3 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10">
-                        Disponibles
-                    </span>
-                </button>
+    const { code, data, message } = response;
+
+    if (code === "COD_OK") {
+      navigate(`/shoeStore`, { state: { storeData: data } });
+    } else {
+      console.error("Error fetching store data:", message);
+    }
+  };
+
+  if (loading) return <Loading />;
+  if (code !== "COD_OK") return <ErrorMessage message={message} />;
+  if (data.length === 0)
+    return <Message message="No hay tiendas disponibles en este momento." />;
+
+  return (
+    <div className="content-center sm:px-48 sm:py-12">
+      <hr className="border-black" />
+      <div className="flex items-center justify-center py-8">
+        <button className="overflow-hidden relative w-72 p-2 h-20 bg-black text-[#faef57] border-none rounded-lg text-4xl font-bold cursor-pointer z-10 group">
+          TIENDAS
+          <span className="absolute w-96 h-60 -top-32 -left-8 bg-[#faef57] rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-left"></span>
+          <span className="text-black group-hover:opacity-100 pl-4 p-3 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10">
+            Disponibles
+          </span>
+        </button>
+      </div>
+      <hr className="border-black" />
+      <div>
+        <p className="text-base py-8 sm:px-48 px-6 text-center">
+          ¡Mira los zapatos que ofrecen diferentes tiendas! <br />
+          Existe gran variedad de tiendas donde podrás ver los zapatos que más
+          te gusten con sus características. <br />
+          No pierdas más el tiempo y revisa, ¡Tus zapatos están esperando por
+          ti!
+        </p>
+      </div>
+      <hr className="border-black" />
+      <div className="flex flex-wrap items-center justify-center gap-8 py-12">
+        {data.map((store) => (
+          <div
+            className="block cursor-pointer"
+            key={store.store_id}
+            onClick={() => handleStoreClick(store.store_id)}
+          >
+            <div className="bg-[#faef57] rounded-xl shadow-sm shadow-black outline outline-white -outline-offset-8">
+              <div className="group overflow-hidden relative duration-500 hover:after:translate-x-18 hover:before:translate-y-9 hover:before:-translate-x-24 hover:rotate-12 flex justify-center items-center h-44 w-60 bg-black rounded-xl outline outline-white -outline-offset-8">
+                <div className="z-10 flex flex-col items-center">
+                  <span className="text-[#faef57] text-center px-4 text-4xl font-bold pt-4">
+                    {store.name}
+                  </span>
+                  <img
+                    className="w-[60px]"
+                    src="https://t4.ftcdn.net/jpg/01/36/55/49/360_F_136554929_JG7RLQNfAKpAQlmRmdV7QhbEM1PDt6k2.jpg"
+                    alt={store.name}
+                  />
+                </div>
+              </div>
             </div>
-
-            <hr className='border-black' />
-
-            <div>
-                <p className='text-base py-8 sm:px-48 px-6 text-center'>
-                    ¡Mira los zapatos que ofrecen diferentes tiendas! <br />
-                    Existe gran variedad de tiendas donde podrás ver los zapatos que más te gusten con sus características. <br />
-                    No pierdas más el tiempo y revisa, ¡Tus zapatos están esperando por ti!
-                </p>
-            </div>
-
-            <hr className='border-black' />
-
-            <div className='flex flex-wrap items-center justify-center gap-8 py-12'>
-                {data.map((store) => (
-                    <Link to={`/store/${store.store_id}`} className="block" key={store.store_id}>
-                        <div className="bg-[#faef57] rounded-xl shadow-sm shadow-black outline outline-white -outline-offset-8">
-                            <div className="group overflow-hidden relative duration-500 hover:after:translate-x-18 hover:before:translate-y-9 hover:before:-translate-x-24 hover:rotate-12 flex justify-center items-center h-44 w-60 bg-black rounded-xl outline outline-white -outline-offset-8">
-                                <div className="z-10 flex flex-col items-center">
-                                    <span className="text-[#faef57] text-center px-4 text-4xl font-bold pt-4">{store.name}</span>
-                                    <img
-                                        className="w-[60px]"
-                                        src="https://t4.ftcdn.net/jpg/01/36/55/49/360_F_136554929_JG7RLQNfAKpAQlmRmdV7QhbEM1PDt6k2.jpg"
-                                        alt={store.name}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </Link>
-                ))}
-            </div>
-        </div>
-    );
-}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default StoresForm;
