436ba36d7ce3bba2423cbcd53395cc3a79607381
GabrielDiaZz <gabriel.diaz.23.02.05@gmail.com>
2024-08-19 12:01:25 -0500

    asasas

--- proyectoPW/src/components/historyUser.jsx
+++ proyectoPW/src/components/historyUser.jsx
@@ -1,54 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useFetchData } from '../hooks/useFectchData';
+import Loading from './messages/loading';
+import ErrorMessage from './messages/errorMessage';
 
 const HistoryUser = () => {
-    return (
-        <div>
-            <div className="my-20">
-                <h1 className="text-center text-4xl font-bold">Historial del usuario</h1>
-                <p className="text-center text-lg">Aqui podrás ver las busquedas que ha realizado el usuario</p>
-            </div>
+  const [userData, setUserData] = useState(null);
+  const { fetchData: { loading, code, message, data } } = useFetchData({ endPoint: "user/getAll" });
 
-            <div>
-                <section className="pb-24 bg-white">
-                    <div className="">
-                        <div className="p-8 sm:p-14 bg-gray-100 rounded-3xl">
-                            
-                            <h2 className="text-center font-medium text-3xl text-black mb-4">Hoy, 08 de agosto del 2024</h2>
+  useEffect(() => {
+    if (data) {
+      setUserData(data);
+    }
+  }, [data]);
 
-                            <div className="">
-                                <div className="">
-                                    <p className="font-medium text-lg leading-8 text-indigo-600">Busquedas</p>
-                                </div>
-                            </div>
+  if (loading) return <Loading />;
+  if (code !== "COD_OK") return <ErrorMessage message={message} />;
+  if (!userData) return <p>No se encontraron datos del usuario.</p>;
 
-
-                            <div className="flex sm:flex-row flex-col sm:gap-20 gap-0">
-                                <div className="my-6 flex-grow">
-                                    <img
-                                        src=""
-                                        alt="imagenZapa"
-                                        className="rounded-xl"
-                                    />
-                                </div>
-
-                                <div className="my-6 flex-grow">
-                                    <h5 className="font-semibold text-xl leading-9 text-black mb-1 ">
-                                        "Nombre del zapato que busco"
-                                    </h5>
-                                </div>
-
-                                <div className="my-6 flex-grow">
-                                    <p className="font-semibold text-xl leading-8 text-black">"Hora que realizo la busqueda"</p>
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
-                </section>
-            </div>
+  return (
+    <div className="flex flex-row gap-12 my-24 px-32">
+      <div id='User' className="w-1/3">
+        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+          <div className="flex flex-col items-center pb-10 py-8">
+            <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="https://st3.depositphotos.com/3538469/15750/i/450/depositphotos_157501024-stock-photo-business-man-icon.jpg" alt="FotoPerfil" />
+            <h5 className="my-2 text-lg font-medium text-black">{userData.name} {userData.lastname}</h5>
+            <span className="my-2 text-sm font-medium text-black">"username"</span>
+            <span className="my-2 text-sm font-medium text-black">{userData.email}</span>
+            <span className="my-2 text-sm font-medium text-black">{userData.fk_category_user}</span>
+          </div>
         </div>
-    )
+      </div>
+
+      <div id='History' className="w-2/3">
+        <div className="mb-8">
+          <h1 className="text-center text-4xl font-bold">Historial del usuario</h1>
+          <p className="text-center text-lg">Aquí podrás ver las búsquedas que ha realizado el usuario</p>
+        </div>
+
+        <div className="">
+          <div className="p-8 bg-gray-100 rounded-3xl">
+            <div className="">
+              <p className="font-semibold text-lg leading-8 text-indigo-600 mb-8">Búsquedas:</p>
+            </div>
+
+            <div className="flex sm:flex-row flex-col sm:gap-20 gap-0">
+              <div className="flex-grow">
+                <img
+                  src=""
+                  alt="imagenZapa"
+                  className="rounded-xl"
+                />
+              </div>
+
+              <div className="flex-grow">
+                <h5 className="font-medium text-lg text-black ">
+                  "Nombre del zapato que busco"
+                </h5>
+              </div>
+
+              <div className="flex-grow">
+                <p className="font-medium text-lg text-black">"Hora que realizó la búsqueda"</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HistoryUser
+export default HistoryUser;
